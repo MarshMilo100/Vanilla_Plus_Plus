c3a8182703a61eb0e2fda68b551d6f58a32065d6
Zach Van Camp <marshmilo100@gmail.com>
2025-09-13 14:13:15 -0500

    update supplimentaries config
    Signed-off-by: Zach Van Camp <marshmilo100@gmail.com>

--- minecraft/start.sh
+++ minecraft/start.sh
@@ -2,8 +2,9 @@
 
 count=0
 
-while [ true ] do
-	echo "Iteration {$count}"
+while [ true ]
+do
+	echo "Iteration $count"
 	"C:\Users\Zach\AppData\Roaming\PrismLauncher\java\java-runtime-delta\bin\java.exe" -Xmx16G -jar fabric-server-mc.1.21.1-loader.0.17.2-launcher.1.1.0.jar nogui
 	count=count+1
 done
